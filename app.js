e58580043f4fc0c40504513fa5734004982d0ff5
Manojsahoo0 <118053423+Manojsahoo0@users.noreply.github.com>
2023-09-07 19:32:35 +0530

    update app

--- app.js
+++ app.js
@@ -1,44 +1,43 @@
-const express = require('express');
-require('dotenv').config()
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const userRouter = require('./Routes/user.js')
-const watchRouter = require("./Routes/watchlist.js")
-const reviewRouter = require('./Routes/review.js')
-const PORT = process.env.PORT || 8000
-const path = require('path'); // for deployment
+const express = require("express");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRouter = require("./Routes/user.js");
+const watchRouter = require("./Routes/watchlist.js");
+const reviewRouter = require("./Routes/review.js");
+const PORT = process.env.PORT || 8000;
+const path = require("path"); // for deployment
 
 const app = express();
-app.use(cors())
+app.use(cors());
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL);
 
-mongoose.connection.on('error',(err)=>{
-    console.log("error")
-})
-mongoose.connection.on('connected',()=>{
-    console.log("connected")
-})
+mongoose.connection.on("error", (err) => {
+  console.log("error");
+});
+mongoose.connection.on("connected", () => {
+  console.log("connected");
+});
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
-app.use("/api/users",userRouter)
-app.use("/api/watchlist",watchRouter)
+app.use("/api/users", userRouter);
+app.use("/api/watchlist", watchRouter);
 
-app.use("/api/review", reviewRouter)
+app.use("/api/review", reviewRouter);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/Client/build")));
-    
-    app.get("*",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'Client','build','index.html'))
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("api running");
+  });
 }
-else{
-    app.get("/", (req,res)=>{
-        res.send("api running")
-    })
-}
-app.listen(PORT,()=>console.log("App started"))
+app.listen(PORT, () => console.log("App started"));
